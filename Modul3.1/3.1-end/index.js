633407da7a8628edab06a5cdaaa475290d6a33db
Dragos Dehelean <34175492+dragosdehelean@users.noreply.github.com>
2018-09-12 21:32:59 +0300

    modul 31-end added

--- Modul3.1/3.1-end/index.js
+++ Modul3.1/3.1-end/index.js
@@ -10,7 +10,7 @@ const bodyParser = require('body-parser');
 
 const mysql = require('mysql');
 
-// First you need to create a connection to the db
+// Conexiunea la baza de date MySQL
 const con = mysql.createConnection({
   host: 'localhost',
   user: 'root',
@@ -27,9 +27,10 @@ MIDDLEWARE
 **********/
 
 // creaza un obiect .body pe req, in care se vor regasi perechile de chei/valori transmise prin formular
-app.use(bodyParser.urlencoded({
-  extended: false
-}));
+app.use(bodyParser.urlencoded({extended: false}));
+
+
+
 
 // permite afisarea asset-urilor din directorul "/public"
 app.use('/static', express.static('public'));
@@ -44,17 +45,19 @@ app.use((req, res, next) => {
 **********/
 
 app.get('/', (req, res) => {
-  res.render('pages/index')
+  res.render('pages/index', {preferinte: "necunoscute" })
 });
 
 app.get('/New-page', (req, res) => {
     
-  con.query('SELECT * FROM employees', (err, results, fields)=>{
+  con.query('SELECT * FROM employees', (err, results, fields) => {
     //console.log(results);   
-    if (!err)
+    if (!err) {
+      
       res.render('pages/new-page', {results: results});
+    }     
     else
-      console.log('Error while performing Query.');
+      console.log('Error while performing Query.' + err);
   });  
   
 });
@@ -67,20 +70,17 @@ app.post('/hello', [
     // username must be an email
     // check('email').isEmail(),
     // password must be at least 2 chars long
-    check('name').isLength({
-      min: 2
-    })
+    // check('name').isLength({      min: 2     })
   ],
   (req, res) => {
-    const errors = validationResult(req);
-    if (!errors.isEmpty()) {
-      return res.status(422).json({
-        errors: errors.array()
-      });
-    }
-    res.render('pages/hello', {
-      name: req.body.name
-    });
+    // const errors = validationResult(req);
+    // if (!errors.isEmpty()) {
+    //   return res.status(422).json({ errors: errors.array() });
+    // }
+    // res.render('pages/hello', {name: req.body.name, email: req.body.email});
+    
+    res.render('pages/hello', {nume: req.body.nume, email: req.body.email})
+
   })
 
 app.listen(5000, () => console.log(`Listening on port: 5000`))
