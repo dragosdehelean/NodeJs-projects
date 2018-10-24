b87a8665ad36fa64f1bea2fce94b088d228f620d
Dragos Dehelean <dragos.dehelean@selenis.ro>
2018-10-24 13:18:29 +0300

    upload

--- Modul5.1/5.1-end/index.js
+++ Modul5.1/5.1-end/index.js
@@ -7,6 +7,11 @@ const app = express(); // initializeaza o aplicatie Express
 const cookieParser = require('cookie-parser'); // citeste datele din cookies
 const expressSession = require('express-session'); // managementul sesiunilor
 const { check, validationResult } = require('express-validator/check'); // validare
+// const morgan = require('morgan');
+
+// const morganBody = require('morgan-body');
+
+
 const fs = require('fs');
 
 const multer = require('multer');
@@ -34,6 +39,9 @@ app.use(express.urlencoded({extended: true}));
 
 app.use(express.json());
 
+// app.use(morgan('combined'));
+// morganBody(app);
+
 /**
  * Middleware-ul pentru citirea si parsarea cookie-urilor
  * Efect: pe obiectul req.cookies vor aparea perechile de chei/valori din cookie-urile setate de aplicatia noastra
@@ -53,22 +61,33 @@ app.use(expressSession({
 // seteaza directorul "/public" pentru a afisa asset-uri statice
 app.use('/static', express.static('public'));
 
-// const storage = multer.diskStorage({
-//   destination: (req, file, cb) => {
-//       cb(null, 'public/uploads');
-//   },
 
-//   filename: (req, file, cb) => {
-//       cb(null, file.originalname)
-//   }
-// });
+/**
+ * Multer configuration
+ */
 
-// const upload = multer({ storage: storage});
-
+// Varianta 1
 // const upload = multer({ dest: 'public/uploads/' });
 
-var storage = multer.memoryStorage();
-var upload = multer({ storage: storage, limits: {fileSize: 25000} })
+
+// Varianta 2
+const storage = multer.diskStorage({
+  destination: (req, file, cb) => {
+      cb(null, 'public/uploads');
+  },
+
+  filename: (req, file, cb) => {
+      cb(null, file.originalname)
+  }
+});
+
+const upload = multer({ storage: storage});
+
+
+// Varianta 3
+
+// var storage = multer.memoryStorage();
+// var upload = multer({ storage: storage, limits: {fileSize: 25000} })
 
 /*******************   
 CUSTOM MIDDLEWARES
@@ -111,34 +130,33 @@ app.get('/hello', (req, res) => {
   });
 });
 
-app.post('/hello', upload.single('foto'), email_valid, name_valid,  
-  (req, res) => {  
-    // console.log(req.file);
+app.post('/hello', upload.single('foto'), email_valid, name_valid, (req, res) => {  
+  console.log(req.file);
+  console.dir(req.body);
 
-    console.log(req.headers['content-type']);
-    
-    // fs.writeFile('./public/test.jpg', req.file.buffer, (err)=>{
-    //   if (err) throw err;
-    //   console.log('The file has been saved!');
-    // })
+  
+  // fs.writeFile('./public/test.jpg', req.file.buffer, (err)=>{
+  //   if (err) throw err;
+  //   console.log('The file has been saved!');
+  // })
 
-    // pune erorile din req in obiectul errors 
-    const errors = validationResult(req);
+  // pune erorile din req in obiectul errors 
+  const errors = validationResult(req);
 
-    // 1) Daca nu exista erori => redirect cu mesaj flash
-    if (errors.isEmpty()) { 
-      req.session.flashMessage = 'Excelent, te-ai inscris cu email-ul ' + req.body.email;
-      res.cookie('nume', req.body.nume)
-      res.redirect('/');
-    }  
-     // 2) Daca exista erori => afiseaza din nou formularul cu mesaje de eroare si datele completate
-     else { 
-      res.render('pages/hello', {
-        data: req.body,
-        errors: errors.mapped()
-      });
-    }
-  });
+  // 1) Daca nu exista erori => redirect cu mesaj flash
+  if (errors.isEmpty()) { 
+    req.session.flashMessage = 'Excelent, te-ai inscris cu email-ul ' + req.body.email;
+    res.cookie('nume', req.body.nume)
+    res.redirect('/');
+  }  
+    // 2) Daca exista erori => afiseaza din nou formularul cu mesaje de eroare si datele completate
+    else { 
+    res.render('pages/hello', {
+      data: req.body,
+      errors: errors.mapped()
+    });
+  }
+});
 
 app.post('/goodbye', (req, res) => {
   res.clearCookie('nume');
